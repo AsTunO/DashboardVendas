10f95769dc5a0beb06fb458e88cfa19aa5d1680c
Júlio Cesar <juliocesar27072004@outlook.com>
2021-11-03 13:26:47 -0300

    Donut Update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
             <BarChart />
           </div>
           <div className="col-sm-6">
-            <h5 className="text-center text-secondary"> Todas vendas</h5>
+            <h5 className="text-center text-secondary"> Todas as vendas</h5>
             <DonutChart/>
           </div>
         </div>
