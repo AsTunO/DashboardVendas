0d8e34422686382e8f7f9a89be38117493d42c1d
Júlio Cesar <juliocesar27072004@outlook.com>
2021-11-05 09:30:06 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,10 +1,25 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">DSVendas</h1>
+                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <hr />
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                    <Link className="btn btn-primary btn-lg" to="/dashboard">
+                        Acessar Dashboard
+                    </Link>
+                </div>
+            </div>
+            <Footer />
+        </>
     );
-}
+} 
 
 export default Home;
